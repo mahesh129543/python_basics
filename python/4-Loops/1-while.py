86d08706ca4061a127724daefbce7af6edfbb61d
Mahesh Narke <maheshnarke882@gmail.com>
2026-08-13 19:59:53 +0530

    new topics

--- python/4-Loops/1-while.py
+++ python/4-Loops/1-while.py
@@ -22,53 +22,127 @@ while condition:
 
 #example
 
+# i=1
+# while i<=10:
+#     print(i,end=" ")
+#     i=i+1
+
+# j=10
+# while j>=1:
+#     print(j,end=" ")
+#     j=j-1
+
+# #print even
+
+
+# print("\n")
+
+# i=1
+
+# while i<=20:
+#     if i%2==0:
+#         print(i,end=" ")
+#     i=i+1
+
+# print("\n")
+
+
+
+# #write program to all natural numbers 1-n
+
+
+
+# n=int(input("enter the number:"))
+# i=1
+# while i<=n:
+#     print(i,end=" ")
+#     i=i+1
+
+# n=int(input("enter the number:"))
+# i=n
+# while i>=1:
+#     print(i,end=" ")
+#     i=i-1
+
+
+
+# i=1
+
+# while i<=50:
+#     if i%2!=0:
+#         print(i)
+#     i=i+1
+
+# i=1
+# cnt=0
+
+# while i<=15:
+#     if i%2==0:
+#         print(i)
+#         cnt+=1
+#     i=i+1
+# print(cnt,"even count")
+
+
+
 i=1
+sum=0
+
 while i<=10:
-    print(i,end=" ")
-    i=i+1
-
-j=10
-while j>=1:
-    print(j,end=" ")
-    j=j-1
-
-#print even
-
-
-print("\n")
-
-i=1
-
-while i<=20:
-    if i%2==0:
-        print(i,end=" ")
-    i=i+1
-
-print("\n")
-
-
-
-#write program to all natural numbers 1-n
-
-
-
-n=int(input("enter the number:"))
-i=1
-while i<=n:
-    print(i,end=" ")
-    i=i+1
-
-n=int(input("enter the number:"))
-i=n
-while i>=1:
-    print(i,end=" ")
-    i=i-1
-
-
-
-i=1
-
-while i<=50:
     if i%2!=0:
         print(i)
+        sum=sum+i
     i=i+1
+print(sum,"odd sum")
+
+#  Q10.Wap take two input and display all odd numbers between them and find sum and count.
+
+num1=int(input("enter the number:"))
+num2=int(input("enter the number:"))
+
+
+if num1<num2:
+    k=num1
+    j=num2
+else:
+    k=num2
+    j=num1
+i=k
+sum=0
+cnt=0
+
+while i<=j:
+    if i%2!=0:
+        print(i)
+        sum=sum+i
+        cnt=cnt+1
+    i=i+1
+print(sum,"odd sum")
+print(cnt,"odd count"   )
+
+
+
+#nested whilw loop
+
+
+i=1
+while i<=5:
+    j=1
+    while j<=i:
+        print("*",end=" ")
+        j=j+1
+    print()
+    i=i+1
+
+
+i = 1
+
+while i <= 5:
+    j = 1
+
+    while j <= 5:
+        print(f"({i},{j})", end=" ")
+        j = j + 1
+
+    print()
+    i = i + 1
---
+++ python/4-Loops/2-for.py
@@ -0,0 +1,118 @@
+# for loop
+
+# defination: a loop that executes a block of code a certain number of times.
+# what is range function
+
+# range function returns a sequence of numbers, starting from 0 by default, and increments by 1 (by default), and stops before a specified number
+# starting number of the sequence by deault is 0 and increment by 1 by default
+
+# syntax:
+
+'''
+for variable in sequence:
+    statement1
+    statement2
+    statement3
+    statement4
+
+'''
+#there are 4-5 imp point in for loop
+#1. initialization
+#2. condition  
+# print/block of code 
+#3. increment
+#4. decrement
+
+
+#for i in range(num):
+    #print(i)
+
+
+#example
+
+# for i in range(1,11):
+#     print(i,end=" ")
+
+
+# for i in range(11,21):
+#     print(i,end=" ")
+
+
+# print("\n")
+
+# for i in range(2,11,2):
+#     print(i,end=" ")
+
+# for i in range(1,11,2):
+#     print(i,end=" ")
+
+# Q1.Display numbers from 1 to 100.
+
+# for i in range(1,101):
+#     print(i,end=" ")
+# # Q2.Display all even numbers from 1 to 100. 
+# for i in range(2,101,2):
+#     print(i,end=" ")
+# # Q3. Write a program to print all natural numbers from 1 to n. - using for loop
+# n=int(input("enter the number:"))
+# for i in range(1,n+1):
+#     print(i,end=" ")
+# # Q4. Write a program to print all natural numbers in reverse (from n to 1). - using for loop
+# for i in range(n,0,-1):
+#     print(i,end=" ")
+
+# # Q5. Write a program to print all odd number between 1 to 100.
+
+# for i in range(1,101,2):
+#     print(i,end=" ")
+
+# Q6. Write a program to find sum of all natural numbers between 1 to n.
+
+n=int(input("enter the number:"))
+sum=0
+for i in range(1,n+1):
+    sum=sum+i   
+print(sum)
+
+# Q7. Write a program to find sum of all even numbers between 1 to n.
+n=int(input("enter the number:"))
+sum=0
+for i in range(2,n+1,2):
+    sum=sum+i   
+print(sum)
+# Q8. Write a program to find sum of all odd numbers between 1 to n.
+n=int(input("enter the number:"))
+sum=0
+for i in range(1,n+1,2):
+    sum=sum+i   
+print(sum)
+# Q9.Wap enter your name and print it five times.
+name=input("enter your name:")
+for i in range(5):
+    print(name)
+# Q10.Wap take two input and display all odd numbers between them and find sum and count.
+n1=int(input("enter the number:")) 
+n2=int(input("enter the number:"))
+sum=0
+cnt=0
+for i in range(n1,n2+1):
+    if i%2!=0:
+        print(i,end=" ")
+        sum=sum+i
+        cnt=cnt+1
+print()
+print(sum)
+print(cnt)
+# Q11.Wap take two inputs and display all even numbers between them and find sum and count.
+n1=int(input("enter the number:")) 
+n2=int(input("enter the number:"))
+sum=0
+cnt=0
+for i in range(n1,n2+1):
+    if i%2==0:
+        print(i,end=" ")
+        sum=sum+i
+        cnt=cnt+1
+print()
+print(sum)
+print(cnt)
