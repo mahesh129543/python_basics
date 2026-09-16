31428bb664d367ba2d9fea2e309db34f1bf23953
Mahesh Narke <maheshnarke882@gmail.com>
2026-09-16 15:27:09 +0530

    new code

--- python/7-Datatypes/5-string.py
+++ python/7-Datatypes/5-string.py
@@ -229,3 +229,286 @@ give index number but if not present it will give error'''
 s="i love india"
 print(s.rindex("i"))
 
+
+
+
+
+#automorphic number
+'''number which is same as its square last digit is same'''
+
+
+
+
+# n = int(input("Enter the number: "))
+
+# sq = n * n
+
+# if str(sq).endswith(str(n)):
+#     print("Automorhic number")
+# else:
+#     print("Not an autorphic number")
+
+
+
+
+
+#center()
+'''center the string using the given length and also 
+add the character in between the string ohterwise it will add the space
+'''
+
+s="mahesh"
+print(s.center(9,"*"))
+
+#isalnum()
+'''check the string is alphanumeric or not
+
+syntax: string.isalnum()
+
+give true or false'''
+
+s="mahesh123"
+print(s.isalnum())
+
+s="mahesh"
+print(s.isalnum())
+
+
+#isalpha()
+'''check the string is alphabetical or not
+
+syntax: string.isalpha()
+
+give true or false'''
+
+s="mahesh"
+print(s.isalpha())
+
+s="mahesh123"
+print(s.isalpha())
+
+
+#isdigit()
+'''check the string is digit or not
+
+syntax: string.isdigit()
+
+give true or false'''
+
+s="123"
+print(s.isdigit())
+
+s="mahesh"
+print(s.isdigit())
+
+#isascii()
+'''check the string is ascii or not
+
+syntax: string.isascii()
+
+give true or false
+
+'''
+
+s="mahesh"
+print(s.isascii())
+
+s=""
+print(s.isascii())
+
+
+#islower()
+'''check the string is lowercase or not
+
+syntax: string.islower()
+
+give true or false'''
+
+s="mahesh"
+print(s.islower())
+
+s="MAHEsh"
+print(s.islower())
+
+
+#isupper()
+'''check the string is uppercase or not
+
+syntax: string.isupper()
+
+give true or false'''
+
+s="MAHEsh"
+print(s.isupper())
+
+s="mahesh"
+print(s.isupper())
+
+#isspace()
+'''check the string is space or not
+
+syntax: string.isspace()
+
+give true or false'''
+
+s=" "
+print(s.isspace())
+
+s="mahesh"
+print(s.isspace())
+
+#istitle()
+'''check the string is title or not
+
+syntax: string.istitle()
+
+give true or false'''
+
+s="Mahesh"
+print(s.istitle())
+
+s="mahesh"
+print(s.istitle())
+
+# join()
+'''join the string using the given character
+
+syntax: string.join(iterable)'''
+
+l=["mahesh","pune","india","at"]
+res=" ".join(l)
+print(res)               
+
+
+# 1. WAP to print all the consonants char from the String.
+s="i love india"
+l=[]
+
+for i in s:
+    if i not in "aeiouAEIOU":
+        l.append(i)
+print(l)
+
+# 2. Print the words which starts from vowels in list.["amayra","rohit","illina","virat"]
+l=["amayra","rohit","illina","virat"]
+
+for i in l:
+    if i[0] in 'aeiouAEIOU':
+        print(i)
+# 3. WAP to convert all the consonants char in lowercase case.
+s="I LOVE INDIA"
+res=""
+for i in s:
+    if i not in "aeiouAEIOU":
+        res=res+i.lower()
+    else:
+        res=res+i
+print(res)
+# 4. WAP to check whether the all strings are in upper case or not:
+s="MAHESH"
+if s==s.upper():
+    print("all strings are in upper case")
+else:
+    print("all strings are not in upper case")
+
+
+# 5.WAP to check whether the all strings are in lower case or not:
+s="MAHESH"
+if s==s.lower():
+    print("all strings are in lower case")
+else:
+    print("all strings are not in lower case")
+
+
+
+s=" i name is mahesh"
+res=""
+for i in s:
+    if i in "aeiouAEIOU":
+        res=res+i.replace(i,"#")
+    elif i in " ":
+        res=res+i
+    else:
+        res=res+i.replace(i,"*")
+print(res)
+# WAP to replace all the even position char to %
+
+
+s="my name is mahesh"
+res=""
+for i in range(len(s)):
+    if i%2==0:
+        res=res+s[i].replace(s[i],"%")
+    elif s[i] in " ":
+        res=res+s[i]
+    else:
+        res=res+s[i]
+print(res)
+
+
+
+#Write a Python program that takes a list of words 
+# # and return the longest word
+
+
+l=["mahesh","pune","india","at"]
+res=""
+
+for i in l:
+    if len(i)>len(res):
+        res=i
+       
+print(res)
+
+
+# and return the longest word and the length of the longest one.
+list1 = ["hii", "hello", "welcome"]
+
+op = []
+
+for i in list1:
+    op.append((i, len(i)))
+
+print(op)
+
+
+#Reverse the words, order should be maintained
+# string="Hello world"
+#output: "olleh dlrow"
+
+s="Hello world"
+res=s.split(" ")
+s1=""
+
+for i in res:
+    s1=s1+" "+(i[::-1])
+
+print(s1)
+
+
+s=" i like python"
+word=s.split(" ")
+res=""
+
+for i in range(len(word)-1,-1,-1):
+    res=res+word[i]+" "
+    
+
+print(res)
+
+l=[1,2,3]
+s=""
+for i in l:
+    s=s+str(i)
+print(s)
+print(type(s))
+
+
+
+s="1,2"
+l=s.split(",")
+l1=[]
+for i in l:
+    l1.append(int(i))
+print(l1)
+    
--- python/7-Datatypes/tempCodeRunnerFile.py
+++ python/7-Datatypes/tempCodeRunnerFile.py
@@ -1,1 +1,8 @@
+n = int(input("Enter the number: "))
 
+# sq = n * n
+
+# if str(sq).endswith(str(n)):
+#     print("Automorhic number")
+# else:
+#     print("Not an autorphic number")
